7828f0231eb55e62633606317ddd333f1f5207c5
Olexii Babii <babiy0617@gmail.com>
2025-08-03 12:43:01 +0300

    fix_2

--- app/main.py
+++ app/main.py
@@ -8,24 +8,35 @@ class Person:
 
 
 def create_person_list(people: list[dict]) -> list:
+    approve = []
     result = []
     for person in people:
         person_object = Person(name=person["name"], age=person["age"])
         result.append(person_object)
     for i, person in enumerate(people):
         if (person.get("husband", False)
-            and person.get("husband", False) is not None
-                and Person.people[person.get("husband")]):
+                and Person.people[person.get("husband")]
+                and person["name"] not in approve):
             result[i].husband = Person.people[person.get("husband")]
+            approve.append(person["name"])
+            result[result.index(Person.people[person.get("husband")])
+                   ].wife = Person.people[person["name"]]
+            approve.append(person["husband"])
         elif (person.get("wife", False)
-              and person.get("wife", False) is not None
-              and Person.people[person.get("wife")]):
+              and Person.people[person.get("wife")]
+              and person["name"] not in approve):
             result[i].wife = Person.people[person.get("wife")]
+            approve.append(person["name"])
+            result[result.index(Person.people[person["wife"]])
+                   ].husband = Person.people[person["name"]]
+            approve.append(person["wife"])
     return result
 
 
-# people = [{"name": "Ross", "age": 30, "wife": "Rachel"},
-#           {"name": "Joey", "age": 29, "wife": None},
-#           {"name": "Rachel", "age": 28, "husband": "Ross"}]
+# people = [
+#     {"name": "Ross", "age": 30, "wife": "Rachel"},
+#     {"name": "Joey", "age": 29, "wife": None},
+#     {"name": "Rachel", "age": 28, "husband": "Ross"}
+# ]
 
 # print(create_person_list(people))
